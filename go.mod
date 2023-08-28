6699685df26a828cca38da798d3593084e2df4ee
ysnikitin <ysnikitin@gmail.com>
2023-08-28 11:23:19 -0400

    need go.sum

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ysnikitin/test-attest
 
 go 1.20
+
+require github.com/google/uuid v1.3.1
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 func main() {
-  fmt.Printf("hi!");
+	fmt.Printf("hi with this uuid: %v", uuid.Must(uuid.NewRandom()).String())
 }
